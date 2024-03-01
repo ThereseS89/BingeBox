a8382eec7f770fd586a2d9cd98aa95dd5f17073d
Therese <thereseskold@live.com>
2024-03-01 15:44:14 +0100

    made the header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,9 +2,15 @@ import './Stylesheets/header.scss'
 
 const Header = () => {
 	return (
-		<>
-		<div>BingeBox</div>
-		</>
+		<header>
+			<div className='nav-hamburger'>
+				<div className='line'></div>
+				<div className='line'></div>
+				<div className='line'></div>
+			</div>
+			<h4>Binge<span>Box</span></h4>
+			<div className='user-container'><p>T</p></div>
+		</header>
 	)
 }
 
