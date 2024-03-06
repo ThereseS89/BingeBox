45448cd1f46c62b8883fc19c383b1f1ddd14311d
Therese <thereseskold@live.com>
2024-03-06 11:46:07 +0100

    made footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,8 +1,18 @@
 import './Stylesheets/footer.scss'
+
+import { FaFacebookF, FaYoutube, FaInstagram } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6'
+
 const Footer = () => {
 	return (
 		<footer>
-		<div>Footer</div>
+			<div className='socialmedia-container'> 
+				<FaFacebookF /> <FaYoutube /><FaInstagram /> <FaXTwitter />
+			</div>
+			<div className='logo-container'>
+				<p className='logotext'>Binge<span>Box</span></p> 
+				<p className='year'>2024</p>
+			</div>
 		</footer>
 	)
 }
