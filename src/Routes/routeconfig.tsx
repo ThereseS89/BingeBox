c1cc1c69197faf4cb150fab025c4834b8892c46c
Therese <thereseskold@live.com>
2024-03-28 23:46:26 +0100

    fixed flex

--- src/Routes/routeconfig.tsx
+++ src/Routes/routeconfig.tsx
@@ -10,7 +10,10 @@ import AboutUs from "./AboutUs/AboutUs"
 import Movies from "./Media/Movies";
 import TvShows from "./Media/TvShows";
 
+
+
 const router = createBrowserRouter([
+
 	{
 		path: "/",
 		element: <Root />,
@@ -41,7 +44,7 @@ const router = createBrowserRouter([
 			},
 			{
 				path: "/mediaPage/:id",
-				element: <MediaPage />
+				element: <MediaPage adult={false} backdrop_path={""} belongs_to_collection={null} budget={0} genres={[]} homepage={""} id={""} imdb_id={""} media_type={""} original_language={""} original_title={""} overview={""} popularity={0} poster_path={""} production_companies={[]} production_countries={[]} release_date={""} revenue={0} runtime={0} spoken_languages={[]} status={""} tagline={""} title={""} video={false} vote_average={0} vote_count={0} seasons={""} PremiereYear={""} first_air_date={""} episode_run_time={""} mediaId={""} imageposter={""} name={""} original_name={""} genre_ids={[]} mediaType={""} profile_path={""} />
 			},
 			{
 				path: "/aboutUs",
